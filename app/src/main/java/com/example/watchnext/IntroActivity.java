2bbde60f56745c19cdf1eaeba5a64fec18dac361
Ran Biderman <bsuro10@gmail.com>
2022-01-15 13:22:53 +0200

    rearrange activities

--- app/src/main/java/com/example/watchnext/IntroActivity.java
+++ app/src/main/java/com/example/watchnext/IntroActivity.java
@@ -1,16 +1,14 @@
 package com.example.watchnext;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 
-import android.app.FragmentManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
     }
 }
