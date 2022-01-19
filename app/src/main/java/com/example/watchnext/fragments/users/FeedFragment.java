f0479bfdf5507623b6d4f2ca905963c9addc1edd
Ran Biderman <bsuro10@gmail.com>
2022-01-20 00:45:41 +0200

    Created feed fragment

---
+++ app/src/main/java/com/example/watchnext/fragments/users/FeedFragment.java
@@ -0,0 +1,20 @@
+package com.example.watchnext.fragments.users;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.watchnext.R;
+
+public class FeedFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+
+}
