2c4024d4342edc62971e1efa119a4be3f0a8b8e6
Ran Biderman <bsuro10@gmail.com>
2022-01-23 23:30:43 +0200

    Created add-review fragment

---
+++ app/src/main/java/com/example/watchnext/fragments/users/AddReviewFragment.java
@@ -0,0 +1,132 @@
+package com.example.watchnext.fragments.users;
+
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.navigation.Navigation;
+
+import com.example.watchnext.R;
+import com.example.watchnext.utils.CameraUtilFragment;
+import com.example.watchnext.utils.InputValidator;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class AddReviewFragment extends CameraUtilFragment {
+
+    private ImageView reviewImageView;
+    private TextInputLayout titleTextInput;
+    private TextInputEditText titleEditText;
+    private TextInputLayout descriptionTextInput;
+    private TextInputEditText descriptionEditText;
+    private MaterialButton backButton;
+    private MaterialButton postButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_review, container, false);
+        initializeMembers(view);
+        setListeners();
+        return view;
+    }
+
+    private void initializeMembers(View view) {
+        reviewImageView = view.findViewById(R.id.add_review_fragment_card_imageview);
+        titleTextInput = view.findViewById(R.id.add_review_fragment_title_text_input);
+        titleEditText = view.findViewById(R.id.add_review_fragment_title_edit_text);
+        descriptionTextInput = view.findViewById(R.id.add_review_fragment_description_text_input);
+        descriptionEditText = view.findViewById(R.id.add_review_fragment_description_edit_text);
+        backButton = view.findViewById(R.id.add_review_fragment_back_arrow_button);
+        postButton = view.findViewById(R.id.add_review_fragment_post_button);
+    }
+
+    private void setListeners() {
+        setPostButtonOnClickListener();
+        setBackButtonOnClickListener();
+        setTitleEditTextOnKeyListener();
+        setDescriptionEditTextOnKeyListener();
+        setReviewImageViewOnClickListener();
+    }
+
+    private void setPostButtonOnClickListener() {
+        postButton.setOnClickListener(view -> {
+            setErrorIfTitleIsInvalid();
+            setErrorIfDescriptionIsInvalid();
+            if(isFormValid()) {
+                post(view);
+            }
+        });
+    }
+
+    private void post(View view) {
+        // TODO
+    }
+
+    private boolean isFormValid() {
+        return (InputValidator.isReviewTitleValid(titleEditText.getText()) &&
+                InputValidator.isReviewDescriptionValid(descriptionEditText.getText()));
+    }
+
+    private void setReviewImageViewOnClickListener() {
+        reviewImageView.setOnClickListener(this::showCameraMenu);
+    }
+
+    private void setBackButtonOnClickListener() {
+        backButton.setOnClickListener(view -> {
+            Navigation.findNavController(view).navigateUp();
+        });
+    }
+
+    private void setTitleEditTextOnKeyListener() {
+        titleEditText.setOnKeyListener((view, i, keyEvent) -> {
+            setErrorIfTitleIsInvalid();
+            return false;
+        });
+    }
+
+    private void setDescriptionEditTextOnKeyListener() {
+        descriptionEditText.setOnKeyListener((view, i, keyEvent) -> {
+            setErrorIfDescriptionIsInvalid();
+            return false;
+        });
+    }
+
+    private void setErrorIfTitleIsInvalid() {
+        if (!InputValidator.isReviewTitleValid(titleEditText.getText())) {
+            titleTextInput.setError(getString(R.string.required_field));
+        } else {
+            titleTextInput.setError(null);
+        }
+    }
+
+    private void setErrorIfDescriptionIsInvalid() {
+        if (!InputValidator.isReviewDescriptionValid(descriptionEditText.getText())) {
+            descriptionTextInput.setError(getString(R.string.required_field));
+        } else {
+            descriptionTextInput.setError(null);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            reviewImageView.setImageBitmap(imageBitmap);
+        } else if (requestCode == REQUEST_OPEN_GALLERY && resultCode == RESULT_OK) {
+            Uri selectedImageUri = data.getData();
+            if (selectedImageUri != null) {
+                reviewImageView.setImageURI(selectedImageUri);
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/watchnext/fragments/users/FeedFragment.java
+++ app/src/main/java/com/example/watchnext/fragments/users/FeedFragment.java
@@ -9,39 +9,70 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.utils.widget.ImageFilterView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.watchnext.R;
 import com.example.watchnext.common.interfaces.OnItemClickListener;
+import com.example.watchnext.fragments.guests.LoginFragmentDirections;
+import com.example.watchnext.models.Model;
 import com.example.watchnext.models.entities.Review;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FeedFragment extends Fragment {
 
+    RecyclerView reviewList;
     SwipeRefreshLayout swipeRefresh;
     ReviewListAdapter reviewListAdapter;
+    FloatingActionButton addReviewActionButton;
+    ImageFilterView logoutImageFilterView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_feed, container, false);
-
-        swipeRefresh = view.findViewById(R.id.feed_fragment_swiperefresh);
+        initializeMembers(view);
+        setListeners();
         swipeRefresh.setOnRefreshListener(() -> {}); // TODO: Modal.instance.refreshStudentList
-
-        RecyclerView reviewList = view.findViewById(R.id.feed_fragment_review_list_rv);
         reviewList.setHasFixedSize(true);
         reviewList.setLayoutManager(new LinearLayoutManager(getContext()));
         reviewListAdapter = new ReviewListAdapter();
         reviewList.setAdapter(reviewListAdapter);
-
         reviewListAdapter.setOnItemClickListener((v, position) -> {
             Log.d("TAG", "Clicked Row in position: " + position);
         });
 
         return view;
+    }
+
+    private void initializeMembers(View view) {
+        addReviewActionButton = view.findViewById(R.id.feed_fragment_add_review_button);
+        swipeRefresh = view.findViewById(R.id.feed_fragment_swiperefresh);
+        reviewList = view.findViewById(R.id.feed_fragment_review_list_rv);
+        logoutImageFilterView = view.findViewById(R.id.feed_fragment_logout_image_view);
+    }
+
+    private void setListeners() {
+        setOnAddReviewActionButtonClickListener();
+        setOnLogoutButtonClickListener();
+    }
+
+    private void setOnLogoutButtonClickListener() {
+        logoutImageFilterView.setOnClickListener(view -> {
+            Model.instance.logout(() -> {
+                Navigation.findNavController(view).navigate(LoginFragmentDirections.actionGlobalLoginFragment());
+            });
+        });
+    }
+
+    private void setOnAddReviewActionButtonClickListener() {
+        addReviewActionButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(FeedFragmentDirections.actionFeedFragmentToAddReviewFragment())
+        );
     }
 
     static class ReviewListViewHolder extends RecyclerView.ViewHolder {
